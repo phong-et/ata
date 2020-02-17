b7c2af1ace11f5ca06aae926a608d89a29858a8a
hemlk <truonglehiep@gmail.com>
2020-02-17 23:37:09 +0700

    error handling when fetching external IP

--- lib/screens/check_in_screen.dart
+++ lib/screens/check_in_screen.dart
@@ -1,33 +1,37 @@
 import 'dart:convert';
+import 'package:ata/util.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
 import 'package:ata/screens/splash-screen.dart';
 
 class CheckInScreen extends StatelessWidget {
   Future<Map<String, dynamic>> getDeviceIP() async {
-    var response = await http.get('http://ip-api.com/json');
-    final responseData = json.decode(response.body) as Map<String, dynamic>;
-
-    return responseData;
+    try {
+      var responseText = await Util.fetch(FetchType.GET, 'http://ip-api.com/json', {});
+      final responseData = json.decode(responseText) as Map<String, dynamic>;
+      return responseData;
+    } catch (error) {
+      return {'error': error};
+    }
   }
 
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
       future: getDeviceIP(),
-      builder: (ctx, AsyncSnapshot<Map<String, dynamic>> snapshot) =>
-          snapshot.connectionState == ConnectionState.waiting
-              ? SplashScreen()
-              : Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      Text(snapshot.data['city']),
-                      Text(snapshot.data['org']),
-                      Text(snapshot.data['query']),
-                    ],
-                  ),
-                ),
+      builder: (ctx, AsyncSnapshot<Map<String, dynamic>> snapshot) => snapshot.connectionState == ConnectionState.waiting
+          ? SplashScreen()
+          : Center(
+              child: snapshot.data['error'] != null
+                  ? Text(snapshot.data['error'].toString())
+                  : Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        Text(snapshot.data['city']),
+                        Text(snapshot.data['org']),
+                        Text(snapshot.data['query']),
+                      ],
+                    ),
+            ),
     );
   }
 }
