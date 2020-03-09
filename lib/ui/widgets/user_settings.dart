e8f0405e9f19827abbe7422666f589f9b66acfc5
tri-et <tripham178@gmail.com>
2020-03-09 11:08:18 +0700

    create user settings/notifier

---
+++ lib/ui/widgets/user_settings.dart
@@ -0,0 +1,70 @@
+import 'package:ata/core/notifiers/user_settings_notifier.dart';
+import 'package:ata/ui/widgets/ata_button.dart';
+import 'package:ata/ui/widgets/base_widget.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+class UserSettings extends StatelessWidget {
+  final displayNameController = TextEditingController();
+  final photoUrlController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return BaseWidget<UserSettingsNotifier>(
+      notifier: UserSettingsNotifier(Provider.of(context)),
+      onNotifierReady: (notifier) => notifier.getUserSettings(),
+      builder: (context, notifier, child) {
+        displayNameController.text = notifier.busy ? 'Loading ...' : notifier.displayName;
+        photoUrlController.text = notifier.busy ? 'Loading ...' : notifier.photoUrl;
+        return Column(
+          children: <Widget>[
+            Text(
+              'User Settings',
+              style: TextStyle(
+                fontWeight: FontWeight.w900,
+                fontStyle: FontStyle.italic,
+                fontSize: 30,
+                color: Colors.green[800],
+                shadows: [Shadow(color: Colors.grey, offset: Offset(1.0, 1.0), blurRadius: 5.0)],
+              ),
+            ),
+            Divider(),
+            TextField(
+              decoration: InputDecoration(labelText: 'Display Name'),
+              keyboardType: TextInputType.number,
+              controller: displayNameController,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Photo Url'),
+              keyboardType: TextInputType.number,
+              controller: photoUrlController,
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                AtaButton(
+                  label: 'Refresh',
+                  onPressed: notifier.busy ? null : () => notifier.getUserSettings(),
+                ),
+                SizedBox(width: 25.0),
+                AtaButton(
+                  label: 'Update',
+                  icon: Icon(Icons.save),
+                  onPressed: notifier.busy
+                      ? null
+                      : () => notifier.saveUserSettings(
+                            displayNameController.text,
+                            photoUrlController.text,
+                          ),
+                )
+              ],
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
