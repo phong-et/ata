9aceb522e0120c5fdb003b63ffbbc9d2c8f09fa8
Phillip <phillip7.et@gmail.com>
2020-03-19 18:07:17 +0700

    fix ata screen widget

--- lib/ui/screens/settings_screen.dart
+++ lib/ui/screens/settings_screen.dart
@@ -6,14 +6,7 @@ class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AtaScreen(
-      body: Card(
-        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
-        elevation: 8.0,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: OfficeSettings(),
-        ),
-      ),
+      body: OfficeSettings(),
     );
   }
 }
--- lib/ui/widgets/ata_screen.dart
+++ lib/ui/widgets/ata_screen.dart
@@ -6,7 +6,17 @@ class AtaScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
-      child: Padding(padding: const EdgeInsets.all(8.0), child: body),
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Card(
+          shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8.0)),
+          elevation: 8.0,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: body,
+          ),
+        ),
+      ),
     );
   }
 }
