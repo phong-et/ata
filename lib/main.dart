2521537e1bfa4a253ee8e2b17e1832331df3683d
tri-et <tripham178@gmail.com>
2020-03-19 11:54:33 +0700

    fix changes requested

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,29 @@
 import 'package:ata/providers.dart';
-import 'package:ata/ui/screens/home_screen.dart';
-import 'package:ata/ui/screens/loading-screen.dart';
-import 'package:ata/ui/screens/login_screen.dart';
 import 'package:ata/ui/widgets/auth_redirect.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:ata/ui/screens/login_screen.dart';
+import 'package:ata/ui/screens/home_screen.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
+class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> with WidgetsBindingObserver {
-  AppLifecycleState _lastLifecycleState;
-
-  @override
-  void initState() {
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    WidgetsBinding.instance.removeObserver(this);
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) async {
-    super.didChangeAppLifecycleState(state);
-    setState(() {
-      _lastLifecycleState = state;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
-    if (_lastLifecycleState == AppLifecycleState.resumed || _lastLifecycleState == null)
-      return MultiProvider(
-        providers: providers,
-        child: MaterialApp(
-          title: 'Flutter Demo',
-          theme: ThemeData(
-            primarySwatch: Colors.green,
-          ),
-          home: AuthRedirect(),
-          routes: {
-            HomeScreen.routeName: (_) => HomeScreen(),
-            LoginScreen.routeName: (_) => LoginScreen(),
-          },
+    return MultiProvider(
+      providers: providers,
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.green,
         ),
-      );
-    return MaterialApp(home: LoadingScreen());
+        home: AuthRedirect(),
+        routes: {
+          HomeScreen.routeName: (_) => HomeScreen(),
+          LoginScreen.routeName: (_) => LoginScreen(),
+        },
+      ),
+    );
   }
 }
