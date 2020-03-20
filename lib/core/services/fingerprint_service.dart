012c074842ee5e73e528ae5daccee0cc23a675b4
tri-et <tripham178@gmail.com>
2020-03-20 12:39:30 +0700

    fingerprint login

---
+++ lib/core/services/fingerprint_service.dart
@@ -0,0 +1,23 @@
+import 'package:ata/core/models/failure.dart';
+import 'package:dartz/dartz.dart';
+import 'package:local_auth/local_auth.dart';
+import 'package:flutter/services.dart';
+
+class FingerPrintService {
+  final LocalAuthentication auth = LocalAuthentication();
+
+  Future<Either<Failure, bool>> authenticate() async {
+    try {
+      bool canCheckBiometrics = await auth.canCheckBiometrics;
+      if (!canCheckBiometrics) return Right(false);
+      bool authenticated = await auth.authenticateWithBiometrics(
+        localizedReason: 'Scan your fingerprint to authenticate',
+        useErrorDialogs: true,
+        stickyAuth: false,
+      );
+      return Right(authenticated);
+    } on PlatformException catch (e) {
+      return Left(Failure(e.message));
+    }
+  }
+}
