8e8b368fefb4b6d80e699786b7ed1b40e346dcf1
hemlk <truonglehiep@gmail.com>
2020-03-24 16:34:49 +0700

    add in ataweb

---
+++ ataweb/ataweb/Models/DateIp.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ataweb.Models
+{
+    public class DateIp
+    {
+        public String ServerDate { get; set; }
+        public String ServerDateTime { get; set; }
+        public String ClientIp { get; set; }
+
+
+    }
+}
