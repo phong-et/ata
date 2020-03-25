1b901b37c73f78c62d2e07987ef3fc079ce1688c
Phillip <phillip7.et@gmail.com>
2020-03-25 22:38:11 +0700

    standardize attendance record model

--- lib/core/models/attendance_record.dart
+++ lib/core/models/attendance_record.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/foundation.dart';
 
 class AttendanceRecord {
-  DateTime timeCheckIn;
-  DateTime timeCheckOut;
+  DateTime checkInTime;
+  DateTime checkOutTime;
   String lateReason;
   String earlyReason;
   String error;
 
   AttendanceRecord({
-    @required this.timeCheckIn,
-    @required this.timeCheckOut,
+    @required this.checkInTime,
+    @required this.checkOutTime,
     @required this.lateReason,
     @required this.earlyReason,
     @required this.error,
@@ -17,8 +17,8 @@ class AttendanceRecord {
 
   factory AttendanceRecord.fromJson(Map<String, dynamic> parsedJson) {
     return AttendanceRecord(
-      timeCheckIn: DateTime.parse(parsedJson['timeCheckIn']),
-      timeCheckOut: DateTime.parse(parsedJson['timeCheckIn']),
+      checkInTime: DateTime.parse(parsedJson['checkInTime']),
+      checkOutTime: DateTime.parse(parsedJson['checkInTime']),
       lateReason: parsedJson['lateReason'],
       earlyReason: parsedJson['earlyReason'],
       error: parsedJson['error'] == null ? null : parsedJson['error'],
@@ -26,8 +26,8 @@ class AttendanceRecord {
   }
   Map<String, dynamic> toJson() {
     return {
-      'timeCheckIn': this.timeCheckIn.toIso8601String(),
-      'timeCheckIn': this.timeCheckOut.toIso8601String(),
+      'checkInTime': this.checkInTime.toIso8601String(),
+      'checkInTime': this.checkOutTime.toIso8601String(),
       'lateReason': this.lateReason,
       'earlyReason': this.lateReason,
       'error': this.error,
