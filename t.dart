cf03856f795f769e0fc9318da1915236799631f3
Phillip <phillip7.et@gmail.com>
2020-03-25 22:50:28 +0700

    add test

--- t.dart
+++ t.dart
@@ -5,7 +5,16 @@ bool isCheckinLate(DateTime startWorkTime, DateTime checkingTime, int acceptable
     return seconds > acceptableLateTime;
     //return checkingTime.difference(startWorkTime).inSeconds > acceptableLateTime;
 }
+bool isCheckinEarly(DateTime startWorkTime, DateTime checkingTime, int acceptableLateTime) {
+    int seconds = checkingTime.difference(startWorkTime).inSeconds;
+    print('seconds:$seconds');
+    print('acceptableLateTime: $acceptableLateTime');
+    return seconds < 0;
+    //return checkingTime.difference(startWorkTime).inSeconds > acceptableLateTime;
+}
 main(){
-  bool a = isCheckinLate(DateTime.now(), DateTime.now().add(Duration(seconds: 901)), 900);
-  print(a);
+  bool isLate = isCheckinLate(DateTime.now().add(Duration(seconds: 900)), DateTime.now().add(Duration(seconds: 900)), 900);
+  print(isLate);
+  bool isEarly = isCheckinEarly(DateTime.now().add(Duration(seconds: 901)), DateTime.now().add(Duration(seconds: 900)), 900);
+  print(isEarly);
 }
