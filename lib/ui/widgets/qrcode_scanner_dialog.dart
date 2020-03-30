946c93543e4d52afd16ff5d1573dedd1524ab891
Phillip <phillip7.et@gmail.com>
2020-03-30 19:40:20 +0700

    add _auth service to qr code scanner

--- lib/ui/widgets/qrcode_scanner_dialog.dart
+++ lib/ui/widgets/qrcode_scanner_dialog.dart
@@ -1,41 +1,66 @@
+import 'package:ata/core/services/auth_service.dart';
 import 'package:ata/ui/screens/login_screen.dart';
 import 'package:encrypt/encrypt.dart' as encrypt;
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:qrscan/qrscan.dart' as scanner;
 
 String secretKey = 'aA123Bb321@8*iPg';
 String authDomain = 'localhost:51457';
+
 class QrcodeScannerDialog extends StatefulWidget {
   @override
   QrcodeScannerDialogState createState() => QrcodeScannerDialogState();
 }
 
-class QrcodeScannerDialogState extends State<QrcodeScannerDialog> {
+class QrcodeScannerDialogState extends State<QrcodeScannerDialog> with WidgetsBindingObserver {
   String scanResult = '';
-  String scanResultStatus = 'has\'t scanned yet';
+  AuthService _authService;
   final encrypter = encrypt.Encrypter(encrypt.AES(encrypt.Key.fromUtf8(secretKey), mode: encrypt.AESMode.cbc));
-  //function that launches the scanner
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _authService = Provider.of<AuthService>(context, listen: false);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    WidgetsBinding.instance.removeObserver(this);
+  }
+
   Future scanQRCode() async {
     String cameraScanResult = await scanner.scan();
     setState(() {
-      scanResult = cameraScanResult;
+      if (cameraScanResult != '') scanResult = decrypt(cameraScanResult);
+      if (isInvalidQrCode(scanResult)) 
+        Navigator.of(context).pop();
     });
   }
 
   String decrypt(String qrCode) {
-    print(qrCode);
+    String decryptedCode;
     if (qrCode != '')
       try {
-        return encrypter.decrypt64(qrCode, iv: encrypt.IV.fromUtf8(secretKey));
+        decryptedCode = encrypter.decrypt64(qrCode, iv: encrypt.IV.fromUtf8(secretKey));
+        decryptedCode = isInvalidQrCode(decryptedCode) ? decryptedCode : 'Error: Invalid QR code (1)';
       } on Exception catch (e) {
-        return 'Error:' + e.toString();
+        decryptedCode = 'Error: Invalid QR code (2)';
+        print(e);
       }
-    return '';
+    return decryptedCode;
+  }
+
+  bool isInvalidQrCode(String qrCode) {
+    return qrCode.contains(authDomain);
   }
 
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      title: Text('Please scan QR code from your office website'),
       content: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
@@ -55,8 +80,8 @@ class QrcodeScannerDialogState extends State<QrcodeScannerDialog> {
                   child: Text("Cancel"),
                   color: Colors.green,
                   textColor: Colors.white,
-                  onPressed: () {
-                    Navigator.of(context).pop();
+                  onPressed: () async {
+                    await _authService.signOut();
                     Navigator.of(context).pushReplacementNamed(LoginScreen.routeName);
                   },
                 ),
@@ -66,7 +91,8 @@ class QrcodeScannerDialogState extends State<QrcodeScannerDialog> {
           Padding(
             padding: const EdgeInsets.all(1.0),
             child: Text(
-              decrypt(scanResult),
+              scanResult.contains('Error') ? scanResult : '',
+              style: TextStyle(color: Colors.red, fontStyle: FontStyle.italic),
             ),
           ),
         ],
