cf5d32cdfde7351b309f2223bc1ab83706143db2
Phillip <phillip7.et@gmail.com>
2020-04-01 16:14:24 +0700

    add migration files after run "Add-Migrate AttendanceRecord" statement

---
+++ ataweb/ataweb/Migrations/20200401074513_AddAttendanceRecordTodb.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ataweb.Migrations
+{
+    public partial class AddAttendanceRecordTodb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AttendanceRecord",
+                columns: table => new
+                {
+                    id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    localId = table.Column<string>(nullable: false),
+                    checkInTime = table.Column<DateTime>(nullable: false),
+                    checkOutTime = table.Column<DateTime>(nullable: false),
+                    lateReason = table.Column<string>(nullable: true),
+                    earlyReason = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AttendanceRecord", x => x.id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AttendanceRecord");
+        }
+    }
+}
