57c972bae8fec983caa31ad033809a5c2fd8660c
Phillip <phillip7.et@gmail.com>
2020-04-06 15:02:41 +0700

    enable null able whole project (C# 8.0)

--- ataweb/ataweb/Models/ApplicationDbContext.cs
+++ ataweb/ataweb/Models/ApplicationDbContext.cs
@@ -8,15 +8,9 @@ namespace ataweb.Models
 {
     public class ApplicationDbContext: DbContext
     {
+        #pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options){}
+        #pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
         public DbSet<AttendanceRecord> AttendanceRecord { get; set; }
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<AttendanceRecord>().HasData(
-                new AttendanceRecord {
-            });
-            //base.OnModelCreating(modelBuilder);
-        }
-
     }
 }
--- ataweb/ataweb/Models/AttendanceRecord.cs
+++ ataweb/ataweb/Models/AttendanceRecord.cs
@@ -8,10 +8,13 @@ namespace ataweb.Models
         public int id { get; set; }
         [Required]
         [Display(Name = "User Id")]
+        #pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
         public string localId { get; set; }
+        #pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
+
         public DateTime checkInTime { get; set; }
         public DateTime? checkOutTime { get; set; }
-        public string lateReason { get; set; }
-        public string earlyReason { get; set; }
+        public string? lateReason { get; set; }
+        public string? earlyReason { get; set; }
     }
 }
