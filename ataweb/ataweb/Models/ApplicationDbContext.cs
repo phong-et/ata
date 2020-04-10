b17dcba2d495314d9de1aa9a6ed8be7df633f833
Phillip <phillip7.et@gmail.com>
2020-04-10 14:01:03 +0700

    disable null ref c# 8.0

--- ataweb/ataweb/Models/ApplicationDbContext.cs
+++ ataweb/ataweb/Models/ApplicationDbContext.cs
@@ -8,9 +8,7 @@ namespace ataweb.Models
 {
     public class ApplicationDbContext: DbContext
     {
-        #pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options):base(options){}
-        #pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
         public DbSet<AttendanceRecord> AttendanceRecord { get; set; }
     }
 }
--- ataweb/ataweb/Models/AttendanceRecord.cs
+++ ataweb/ataweb/Models/AttendanceRecord.cs
@@ -5,16 +5,16 @@ namespace ataweb.Models
     public class AttendanceRecord
     {
         [Key]
-        public int id { get; set; }
+        public int Id { get; set; }
         [Required]
         [Display(Name = "User Id")]
         #pragma warning disable CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
-        public string localId { get; set; }
+        public string LocalId { get; set; }
         #pragma warning restore CS8618 // Non-nullable field is uninitialized. Consider declaring as nullable.
 
-        public DateTime checkInTime { get; set; }
-        public DateTime? checkOutTime { get; set; }
-        public string? lateReason { get; set; }
-        public string? earlyReason { get; set; }
+        public DateTime CheckInTime { get; set; }
+        public DateTime? CheckOutTime { get; set; }
+        public string? LateReason { get; set; }
+        public string? EarlyReason { get; set; }
     }
 }
