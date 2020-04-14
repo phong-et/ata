f31571be13ecd09adb2ea98410c216dd44fb33ca
Phillip <phillip7.et@gmail.com>
2020-04-14 23:01:05 +0700

    add record to db demo and migrate attendance record

--- ataweb/ataweb/Controllers/UserController.cs
+++ ataweb/ataweb/Controllers/UserController.cs
@@ -1,22 +1,59 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using ataweb.Data;
+using ataweb.Models;
 
 namespace ataweb.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class UserController
+    public class UserController: Controller
     {
-        [HttpGet("checkin")]
-        public string test()
+        private readonly AtaDbContext _context;
+
+        public UserController(AtaDbContext context)
         {
-            return "{\"test\":1234}";
+            _context = context;
+        }
+
+        [HttpGet("checkin")]
+        public async Task<string> test(string jsonCheckIn)
+        {
+            string result, msg = "";
+            try
+            {
+                var record = new AttendanceRecord
+                {
+                    //Id = "asasas",
+                    CheckInTime = DateTime.Now,
+                    LocalId = "1234",
+                    LateReason = "Late Reason"
+                };
+                if (ModelState.IsValid)
+                {
+                    _context.Add(record);
+                    await _context.SaveChangesAsync();
+                    result = "{\"success\":true, \"msg\":\"\"}";
+                }
+            }
+            catch (DbUpdateException ex)
+            {
+                ModelState.AddModelError("", "Unable to save changes. " +
+                    "Try again, and if the problem persists " +
+                    "see your system administrator.");
+                msg = ex.ToString();
+            }
+            result = $"{{\"success\":false, \"msg\":\"{msg}\"}}";
+            return result;
         }
         [HttpGet("checkout")]
-        public string test1()
+        string test1()
         {
             return "{\"test1\":4321}";
         }
---
+++ ataweb/ataweb/Migrations/20200414151017_AttendanceRecord.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ataweb.Migrations
+{
+    public partial class AttendanceRecord : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "AttendanceRecord",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LocalId = table.Column<string>(nullable: false),
+                    CheckInTime = table.Column<DateTime>(nullable: false),
+                    CheckOutTime = table.Column<DateTime>(nullable: true),
+                    LateReason = table.Column<string>(nullable: true),
+                    EarlyReason = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_AttendanceRecord", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "AttendanceRecord");
+        }
+    }
+}
